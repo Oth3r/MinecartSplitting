b2e70610914fa186067dd534429a27b2604db63e
Oth3r <oth3rone@outlook.com>
2024-12-03 11:28:09 -0600

    1.21.4 port

--- fabric/src/main/java/one/oth3r/minecartsplitting/ModDataGenerator.java
+++ fabric/src/main/java/one/oth3r/minecartsplitting/ModDataGenerator.java
@@ -4,8 +4,8 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.server.recipe.RecipeExporter;
-import net.minecraft.data.server.recipe.RecipeGenerator;
+import net.minecraft.data.recipe.RecipeExporter;
+import net.minecraft.data.recipe.RecipeGenerator;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryKey;
@@ -29,8 +29,8 @@ public class ModDataGenerator implements DataGeneratorEntrypoint {
 		}
 
 		@Override
-		protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup registryLookup, RecipeExporter exporter) {
-			return new RecipeGenerator(registryLookup, exporter) {
+		protected RecipeGenerator getRecipeGenerator(RegistryWrapper.WrapperLookup wrapperLookup, RecipeExporter recipeExporter) {
+			return new RecipeGenerator(wrapperLookup, recipeExporter) {
 				@Override
 				public void generate() {
 					createShapeless(RecipeCategory.MISC, Items.TNT).input(Items.TNT_MINECART)
